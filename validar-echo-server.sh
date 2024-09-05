a1771b66ee31ba5ac96fc2a734866e64b680aec6
IgnacioLosc <iloscocco@fi.uba.ar>
2024-09-05 11:26:23 -0300

    Remover container despues de ejecutar validacion

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,3 +1,3 @@
 # docker exec netcat ./validate.sh
 # docker run --rm -dit --name netcat $(docker build -q ./netcat) && docker exec netcat ./validate.sh
-docker run --rm -dit --name netcat --net tp0_testing_net $(docker build -q ./netcat) && docker exec netcat ./validate.sh
+docker run --rm -dit --name netcat --net tp0_testing_net $(docker build -q ./netcat) && docker exec netcat ./validate.sh && docker stop netcat && docker rm netcat
